aeca8eb679542b580ec8d4fa856c97d824f1e12f
cidmesa <chrizian06@gmail.com>
2025-06-07 22:30:34 +0800

    changed api baseurl for frontend

--- rizal-chatbot/lib/api.ts
+++ rizal-chatbot/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://full-stack-rizal-deployment.onrender.com/api/',
 });
 
 // Add Authorization header automatically
