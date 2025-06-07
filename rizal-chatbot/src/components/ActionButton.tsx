ce7c7b8712b7c0e8cef6cbb00460ec07c49ca067
ethanny <145535621+ethanny@users.noreply.github.com>
2025-06-07 18:12:46 +0800

    added auth ui

---
+++ rizal-chatbot/src/components/ActionButton.tsx
@@ -0,0 +1,45 @@
+export enum ButtonVariant {
+  Filled,
+  Outlined,
+}
+
+export enum ButonSize {
+  Stretched,
+  Compact,
+}
+
+interface ActionButtonProps {
+  label: string;
+  variant?: ButtonVariant;
+  size?: ButonSize;
+  color?: string;
+  onClick: () => void;
+}
+
+export default function ActionButton({
+  label,
+  variant = ButtonVariant.Filled,
+  size = ButonSize.Stretched,
+  color = "blue",
+  onClick,
+}: ActionButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      style={{ borderColor: `var(--color-${color})` }}
+      className={`
+        p-2
+        text-lg font-pica
+        rounded-lg border-2
+        transition-all
+        hover:cursor-pointer
+        ${variant === ButtonVariant.Outlined ? `text-${color}` : `text-white`}
+        ${variant === ButtonVariant.Filled ? `bg-${color}` : `bg-transparent`}
+        ${variant === ButtonVariant.Filled ? `hover:opacity-80` : `hover:bg-${color} hover:text-white`}
+        ${size === ButonSize.Stretched ? "w-full" : "w-fit"}
+      `}
+    >
+      {label}
+    </button>
+  );
+}
--- rizal-chatbot/src/components/LoginModal.tsx
+++ rizal-chatbot/src/components/LoginModal.tsx
@@ -1,40 +1,108 @@
-'use client';
-import { useState } from 'react';
-import API from '@/lib/api';
+"use client";
+import { useState } from "react";
+import API from "@/lib/api";
+import ActionButton, { ButtonVariant } from "./ActionButton";
+import TextField from "./TextField";
 
-export default function LoginModal({ onClose, onLogin }: any) {
-  const [form, setForm] = useState({ username: '', password: '' });
-  const [error, setError] = useState('');
+export default function LoginModal({ onSignUp, onLogin }: any) {
+  const [form, setForm] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
 
   const login = async () => {
     try {
-      const res = await API.post('token/', form);
-      localStorage.setItem('access_token', res.data.access);
+      const res = await API.post("token/", form);
+      localStorage.setItem("access_token", res.data.access);
       onLogin(); // proceed to chat page or dashboard
     } catch (err: any) {
-      setError('Invalid credentials');
+      setError("Invalid credentials");
     }
   };
 
   return (
-    <div className="modal bg-white p-4 shadow-md rounded">
-      <h2 className="text-xl mb-2">Login</h2>
-      <input
-        placeholder="Username"
-        value={form.username}
-        onChange={(e) => setForm({ ...form, username: e.target.value })}
-        className="border p-2 w-full mb-2"
+    <div
+      className="
+        flex flex-col
+        w-full
+        p-5
+        gap-5
+      "
+    >
+      <br />
+
+      <form
+        onSubmit={login}
+        className="
+          flex flex-col
+          gap-5
+        "
+      >
+        <h2
+          className="
+            text-blue text-2xl font-maragsa
+          "
+        >
+          Sign In
+        </h2>
+
+        {error && (
+          <p
+            className="
+              text-center font-pica text-lg text-red
+            "
+          >
+            {error}
+          </p>
+        )}
+
+        <TextField
+          placeholder="Username"
+          value={form.username}
+          onChange={(e) => setForm({ ...form, username: e })}
+        />
+
+        <TextField
+          placeholder="Password"
+          value={form.password}
+          type="password"
+          onChange={(e) => setForm({ ...form, password: e })}
+        />
+
+        <ActionButton label="Login" onClick={login} />
+      </form>
+
+      <br />
+
+      <div
+        className="
+          flex
+          items-center gap-4
+        "
+      >
+        <hr
+          className="
+            flex-1
+            border-1 border-blue
+          "
+        />
+        <p
+          className="
+            text-center font-pica text-lg text-blue
+          "
+        >
+          New to RizalGPT?
+        </p>
+        <hr
+          className="
+            flex-1
+            border-1 border-blue
+          "
+        />
+      </div>
+      <ActionButton
+        label="Sign Up"
+        variant={ButtonVariant.Outlined}
+        onClick={onSignUp}
       />
-      <input
-        placeholder="Password"
-        type="password"
-        value={form.password}
-        onChange={(e) => setForm({ ...form, password: e.target.value })}
-        className="border p-2 w-full mb-2"
-      />
-      {error && <p className="text-red-500">{error}</p>}
-      <button className="bg-blue-500 text-white p-2 mt-2" onClick={login}>Login</button>
-      <button className="text-sm text-blue-600 mt-2" onClick={onClose}>Cancel</button>
     </div>
   );
 }
